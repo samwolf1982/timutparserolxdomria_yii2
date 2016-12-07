66277a8bc3e646ac421b1ca248e327c64e4cf8ec
samwolf1982 <biryukwolf@yahoo.com>
2016-12-07 05:54:16 +0200

    empty pro with admin

---
+++ console/migrations/m161207_035002_create_table_rooms.php
@@ -0,0 +1,29 @@
+<?php
+
+use yii\db\Migration;
+
+class m161207_035002_create_table_rooms extends Migration
+{
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m161207_035002_create_table_rooms cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use safeUp/safeDown to run migration code within a transaction
+    public function safeUp()
+    {
+    }
+
+    public function safeDown()
+    {
+    }
+    */
+}
